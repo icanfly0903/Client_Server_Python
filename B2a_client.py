1414bdb3b408048942c3affb170c30253bb7a17f
Grela <thegrela134@gmail.com>
2021-01-26 00:05:48 +0700

    Add up-download file in client-server socket

---
+++ B2a_client.py
@@ -0,0 +1,125 @@
+# -*- encoding: utf-8 -*-
+import os
+import socket
+import time
+
+# PORT = 9669 # Port to connect server
+
+# Connect server
+# def choose_server():
+#     global SERVER_ADDRESS 
+#     SERVER_ADDRESS = input("Please enter the Server address:")
+
+# Create client
+
+
+class create_client:
+    def __init__(self, SERVER_ADDRESS, PORT):
+        self.SERVER_ADDRESS = SERVER_ADDRESS
+        self.PORT = PORT
+        global client_sock
+        print("Openning client socket...")
+        client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+        server_addr = (SERVER_ADDRESS, PORT)
+
+        # Trying to connect to Server socket
+        try:
+            client_sock.connect(server_addr)
+            print(f"=> Connected to {str(server_addr)}...")
+        except:
+            print("Unable to connect")
+            exit(0)
+    
+    # Hiển thị tất cả các file đã upload lên server
+    def lists(self):
+        print("List files were uploaded to server:")
+        for f in os.listdir("./client<127.0.0.1><9669>/upload/"):
+            print(f"+) {f}")
+    
+    # Upload file lên server 
+    def upload_file(self):
+        request = "upload"
+        client_sock.send(request.encode("utf8"))
+        print("=> Uploading file to server...")
+        # Getting file details
+        file_name = input("Upload file name:")
+        file_size = os.path.getsize("./client<127.0.0.1><9669>/upload/" + file_name)
+        print(file_size)
+        # Sending file_name and detail
+        client_sock.send(file_name.encode("utf8"))
+        client_sock.send(str(file_size).encode("utf8"))
+
+        # Openning file and send data
+        with open("./client<127.0.0.1><9669>/upload/" + file_name, "rb") as f:
+            c = 0
+            # Starting the time capture 
+            start_time = time.time()
+
+            # Running loop while c != file_size 
+            while c <= file_size:
+                data = f.read(10240) 
+                if not data:
+                    break
+                client_sock.sendall(data)
+                c += len(data)
+
+            # Ending the time capture 
+            end_time = time.time()
+
+        print("Tranfer file complete. Total time:", end_time - start_time)
+
+    # Download file từ server 
+    def download_file(self):   
+        request = "download"
+        client_sock.send(request.encode("utf8"))
+        print("=> Downloading file from server...")
+        
+        # File name need to be downloaded
+        file_name = input("Download file name:")
+        client_sock.send(file_name.encode("utf8"))
+        file_size = client_sock.recv(1024).decode("utf8")
+        
+        # Openning and reading file
+        with open("./client<127.0.0.1><9669>/download/" + file_name, "wb") as f:
+            c = 0
+            start_time = time.time()
+
+            # Running the loop while file is received
+            while c <= int(file_size):
+                data = client_sock.recv(10240) # Able to up and down file 10GB
+                if not data:
+                    break
+                f.write(data)
+                c += len(data)
+
+            end_time = time.time()
+
+        print("File transfer complete. Total time:", end_time - start_time)
+    
+    def quit(self):
+        request = "quit"
+        client_sock.send(request.encode("utf8"))
+        print("...close socket")
+        client_sock.close()
+
+
+if __name__ == "__main__":
+    client = create_client('127.0.0.1', 9669)
+    client.lists()
+    while True:
+        print(("-" * 10) + "OPTION" + ("-" * 10))
+        print("\n1. Upload file to server")
+        print("\n2. Download file from server")
+        print("\n3. Close socket")
+        print("\n" + "-" * 26)
+        option = int(input("Please Enter Your Choice:"))
+        if option == 1:
+            client.upload_file()
+        elif option == 2:
+            client.download_file()
+        elif option == 3:
+            client.quit()
+            break
+        else:
+            print("No option! Please Choose Again!")
+        
---
+++ B2a_server.py
@@ -0,0 +1,93 @@
+import os
+import socket
+import time
+
+PORT = 9669 # Port to connect server
+
+def get_info():
+    global HOST_NAME
+    HOST_NAME = socket.gethostname()
+
+    global HOST_ADDRESS
+    HOST_ADDRESS = '127.0.0.1'
+    # socket.gethostbyname(HOST_NAME)
+
+    print("Server Name:", HOST_NAME)
+    print("Server Address:", HOST_ADDRESS)
+
+def create_server():
+    print("Openning socket...")
+    # create server socket
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as server:
+        server.bind((HOST_ADDRESS,PORT))
+        server.listen()
+
+        # Accepting the connection 
+        client_connection, client_address = server.accept()
+        print("...connected to Client", client_address)
+        with client_connection:
+            while True:
+                # Receive request from client
+                request = client_connection.recv(1024).decode("utf8")
+                if request == "download":
+                    print("*** Sending file to client ***")
+                    # Getting file details 
+                    file_name = client_connection.recv(1024).decode("utf8")
+                    if not file_name:
+                        break
+                    else:
+                        print(f"=> {file_name} is being sent to client...")
+                        file_size = os.path.getsize("./server/" + file_name)
+                        client_connection.send(str(file_size).encode("utf8"))
+
+                        # Openning file and sending data 
+                        with open("./server/" + file_name, "rb") as f:
+                            c = 0
+                            # Starting the time capture 
+                            start_time = time.time()
+
+                            # Running loop while c != file_size 
+                            while c <= file_size:
+                                data = f.read(10240) 
+                                if not data:
+                                    break
+                                client_connection.sendall(data)
+                                c += len(data)
+
+                            # Ending the time capture 
+                            end_time = time.time()
+
+                        print("File transfer complete. Total time:", end_time - start_time)
+                    break
+                    
+                elif request == "upload":
+                    print("*** Receiving file from client ***")
+                    file_name = client_connection.recv(1024).decode("utf8")
+                    file_size = client_connection.recv(1024).decode("utf8")
+
+                    if not file_name:
+                        break
+                    else:
+                        with open("./server/" + file_name, "wb") as f:
+                            print(f"=> {file_name} is being received from client ...")
+                            c = 0
+                            start_time = time.time()
+                            # Running the loop while file is received
+                            while c <= int(file_size):
+                                data = client_connection.recv(10240) # Able to up and down file 10GB
+                                if not data:
+                                    break
+                                f.write(data)
+                                c += len(data)
+
+                            end_time = time.time()
+                        
+                        print("File transfer complete. Total time:", end_time - start_time)
+                    break
+                
+                elif request == "quit":
+                    break
+
+if __name__ == "__main__":
+    get_info()
+    create_server()
